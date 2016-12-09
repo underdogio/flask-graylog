40cd066236ecb6e7873e39e8bbe5e6631463ad6d
brettlangdon <brett@blangdon.com>
2016-12-09 09:26:47 -0500

    Bump version to 1.1.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='Flask-Graylog',
-    version='1.0.0',
+    version='1.1.0',
     url='https://github.com/underdogio/flask-graylog.git',
     license='MIT',
     author='Brett Langdon',
