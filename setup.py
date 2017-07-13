702823a4fe4ff468c4a8adb43f0ebd8776970285
brettlangdon <brett@blangdon.com>
2017-07-13 14:46:21 -0400

    Update project for new pypi.org API

--- setup.py
+++ setup.py
@@ -1,13 +1,20 @@
 from setuptools import setup
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.rst')) as f:
+    long_description = f.read()
 
 setup(
     name='Flask-Graylog',
-    version='1.1.1',
+    version='1.1.2',
     url='https://github.com/underdogio/flask-graylog.git',
     license='MIT',
     author='Brett Langdon',
     author_email='me@brett.is',
     description='Configure Graylog logging handlers and middleware for your Flask app',
+    long_description=long_description,
     py_modules=['flask_graylog'],
     zip_safe=False,
     include_package_data=True,
